2eb5e441459d1eb34d79497597356dee885172d2
franklinux <universomacroexpande@gmail.com>
2024-09-20 14:38:25 -0300

    Variaveis, operadores, operadores de comparaçao, aprendidos

---
+++ index.js
@@ -0,0 +1,28 @@
+
+/*Variaveis */
+console.log('Hello, World');
+let nome = "Joao";
+const idade = 20;
+console.log(nome);
+console.log(idade);
+
+let cidade = "Sao Paulo";
+let temperatura = 25;
+let estaChovendo = false;
+console.log(cidade)
+console.log(temperatura)
+console.log(estaChovendo)
+
+/*Operadores */
+let a = 10
+let b = 5
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+/*Operadores de comparçao */
+console.log(10 == "10")
+console.log(10 === "10")
+console.log(10 != "10")
+console.log(10 !== "10")
