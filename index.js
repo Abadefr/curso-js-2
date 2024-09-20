cbbb741879c347ae4455fdeab90c40e349e686b6
franklinux <universomacroexpande@gmail.com>
2024-09-20 16:35:35 -0300

    Testando e aprendendo funçoes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-/*Variaveis */
+/*Variaveis 
 console.log('Hello, World');
 let nome = "Joao";
 const idade = 20;
@@ -13,7 +13,7 @@ console.log(cidade)
 console.log(temperatura)
 console.log(estaChovendo)
 
-/*Operadores */
+//Operadores 
 let a = 10
 let b = 5
 console.log(a + b)
@@ -21,8 +21,35 @@ console.log(a - b)
 console.log(a * b)
 console.log(a / b)
 
-/*Operadores de comparçao */
+//Operadores de comparçao 
 console.log(10 == "10")
 console.log(10 === "10")
 console.log(10 != "10")
 console.log(10 !== "10")
+
+//funçoes 
+function dizerOla(){
+    console.log("Ola, Mundo!");
+}
+
+dizerOla()
+
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(5, 3));
+console.log(somar(10, 20));
+ 
+//Funçao sem retorno 
+function dizerOla(nome) {
+    console.log("Ola, " + nome)
+}
+dizerOla("Ana");
+dizerOla("Pedro") 
+
+function divd(a, b){
+    return a / b
+}
+
+console.log(divd(6, 7))
+console.log(divd(600, 9))*/
